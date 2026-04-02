ae4e9c9642eda894423aaa8f5baa0f5cd1c0c6d7
yuchenzhang123 <3107487769@qq.com>
2026-04-02 16:16:37 +0800

    环境变量修正

--- LMS-SYSTEM-master/src/config.js
+++ LMS-SYSTEM-master/src/config.js
@@ -9,7 +9,7 @@ const ssoGuardEnabled = parseBoolean(process.env.VUE_APP_ENABLE_SSO_GUARD, true)
 
 export const APP_CONFIG = {
   // 从浏览器读取Cookie时使用的名称
-  COOKIE_NAME: process.env.VUE_APP_COOKIE_NAME || 'SSOToken',
+  COOKIE_NAME: process.env.VUE_APP_COOKIE_NAME,
   // 发送到SSO服务器时请求头中使用的Cookie名称
   SSO_COOKIE_NAME: 'WSSOP_SSO_COOKIE_V1',
   EXTERNAL_LOGIN_URL: process.env.VUE_APP_SSO_LOGIN_URL,

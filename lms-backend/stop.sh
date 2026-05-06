74bca0fe5e0126605768b757f8fe6f81248d7f44
yuchenzhang123 <3107487769@qq.com>
2026-05-06 10:06:31 +0800

    chore: 删除旧版启停脚本 start.sh / stop.sh，已由 start-prod.sh / stop-prod.sh 替代

--- lms-backend/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-JAR_NAME="lms-backend-1.0.0.jar"
-PID=$(ps -ef | grep $JAR_NAME | grep -v grep | awk '{print $2}')
-if [ -n "$PID" ]; then
-    kill -9 $PID
-    echo "已停止进程：$PID"
-else
-    echo "未运行"
-fi
